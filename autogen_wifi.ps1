36e7eda0c56e0841f2223c1e9f1d1548ab2e1333
the-one-with-raspberry <victorstefan.costin@gmail.com>
2023-08-23 11:02:13 +0300

    fix

--- autogen_wifi.ps1
+++ autogen_wifi.ps1
@@ -7,4 +7,4 @@ $netconnname = Get-NetConnectionProfile
 $netconnname = $netconnname.Name
 $netconnpass = netsh.exe wlan show profile name="$($Script:netconnname)" key=clear
 $netconnpass = $netconnpass[32].Substring(("    Key Content            : ".Length))
-"country=$($countrycode)`nupdate_config=1`nctrl_interface=/var/run/wpa_supplicant`n`nnetwork={`n`tssid=`"$($Script:netconnname)`"`n`tpsk=`"$($Script:netconnpass)`"`n}" | Out-File -LiteralPath (Join-Path -Path $dev -ChildPath "wpa_supplicant.conf")
+"country=$($countrycode)`nupdate_config=1`nctrl_interface=/var/run/wpa_supplicant`n`nnetwork={`n`tscan_ssid=1`n`tssid=`"$($Script:netconnname)`"`n`tpsk=`"$($Script:netconnpass)`"`n}" | Out-File -LiteralPath (Join-Path -Path $dev -ChildPath "wpa_supplicant.conf")
